ca67604dfc5cac6508d9b74ca4b63c6732b95f5a
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-06-29 12:37:56 +0530

    VComments, Variables

---
+++ 01 Variables & Datatypes/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "Comments & Variables"; ?></title>
+</head>
+<body>
+    <?php 
+    //  This is a single line comment in PHP 
+    /*This 
+    is a multiple line Comment
+    in PHP */
+    echo "Bismillah <br>";
+    ?>
+
+    <?php
+    // Variables
+    $person = 'Boy';
+    $age = 24;
+    $AGE = 'right';
+
+    echo "This $person is $age yrs old $AGE"
+
+    ?>
+</body>
+</html>
